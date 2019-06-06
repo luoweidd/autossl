8e0241b01624f0294467ef49e85d0c3e90db6e36
devops <3245554@qq.com>
2019-06-06 16:22:24 +0800

    tow day

--- ACME/request_certificate.py
+++ ACME/request_certificate.py
@@ -348,10 +348,11 @@ def request_certificate(config, urls, nonce, location):
 # Main Program Function
 ############################################################
 
-def main(config, debug=0):
+
+def cert_request_authorizations(g_debug=0):
 	""" Main Program Function """
 	acme_urls = load_acme_urls(acme_path)
-
+	acme_config = load_acme_parameters(g_debug)
 	url = acme_urls["newAccount"]
 
 	nonce = acme_get_nonce(acme_urls)
@@ -366,7 +367,7 @@ def main(config, debug=0):
 
 	info = json.loads(resp)
 
-	if debug is not 0:
+	if g_debug is not 0:
 		print('')
 		print('Returned Data:')
 		print('##################################################')
@@ -381,7 +382,7 @@ def main(config, debug=0):
 	print('Created At:', info['createdAt'])
 	print('Status:   ', info['status'])
 
-	nonce, resp, order_location = request_certificate(config, acme_urls, nonce, account_url)
+	nonce, resp, order_location = request_certificate(acme_config, acme_urls, nonce, account_url)
 
 	print('Order Location:', order_location)
 	print('')
@@ -393,11 +394,72 @@ def main(config, debug=0):
 	print('Saving certificate request response in cert_request.data')
 	with open('cert_request.data', "w+") as f:
 		f.write(resp)
+	return {"nonce":nonce,"resp":resp,"order_location":order_location}
 
 ############################################################
 # Request an SSL certificate from the ACME server
 ############################################################
+	
 
-acme_config = load_acme_parameters(g_debug)
+def authorizations_cert_request_info():
+	curl = cert_request_authorizations(g_debug)["authorizations"]
+	reps = requests.request('GET',curl)
+	for authorizations_type in reps["challenges"]:
+		if authorizations_type["type"] == "dns-01":
+			return authorizations_type["token"]
 
-main(acme_config, g_debug)
+def token_challenges():
+	token=authorizations_cert_request_info()
+	signature = myhelper.sign(token, AccountKeyFile)
+	return signature
+
+def dns_authorizations():
+	url=cert_request_authorizations(g_debug)["authorizations"]
+	nonce=acme_get_nonce()
+	location=get_account_url()
+
+	payload={}
+	payload_b64 = myhelper.b64(json.dumps(payload).encode("utf8"))
+
+	body_top = {
+		"alg": "RS256",
+		"kid": location,
+		"nonce": nonce,
+		"url": url
+	}
+
+	body_top_b64 = myhelper.b64(json.dumps(body_top).encode("utf8"))
+
+	#
+	# Create the message digest
+	#
+
+	data = "{0}.{1}".format(body_top_b64, payload_b64).encode("utf8")
+
+	signature = myhelper.sign(data, AccountKeyFile)
+
+	jose = {
+		"protected": body_top_b64,
+		"payload": payload_b64,
+		"signature": signature
+	}
+
+	try:
+		print('Calling endpoint:', url)
+		resp = requests.post(url, json=jose, headers=headers)
+	except requests.exceptions.RequestException as error:
+		resp = error.response
+		print(resp)
+	except Exception as error:
+		print(error)
+
+	if resp.status_code < 200 or resp.status_code >= 300:
+		print('Error calling ACME endpoint:', resp.reason)
+		print('Status Code:', resp.status_code)
+		myhelper.process_error_message(resp.text)
+		sys.exit(1)
+
+
+	# resp.text is the returned JSON data describing the new order
+
+	return resp
