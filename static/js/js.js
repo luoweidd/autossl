4e598718cee08649d21e26f80ebba485506bf933
devops <3245554@qq.com>
2019-07-09 10:41:53 +0800

    第十7天，整站调试,添加登录错误消息提示,消息bug(线上公测)。 最后一步：上线。

--- static/js/js.js
+++ static/js/js.js
@@ -176,7 +176,7 @@ function login() {
             //var result= JSON.parse(result)
             var validation = $("#validationres");
             validation[0].style.visibility ="visible";
-            validation.html('<p style="color: red; background-color: white; width: 400px; height: auto; margin: auto; opacity: 0.9; margin-top: 26%">'+result.msg+'</p>');
+            //validation.html('<p style="color: red; background-color: white; width: 400px; height: auto; margin: auto; opacity: 0.9; margin-top: 26%">'+result.msg+'</p>');
             var close_window = $('#close_window');
             close_window[0].style.visibility='visible';
             if (result.msg.redirectUrl !=undefined && result.msg.redirectUrl != null){
